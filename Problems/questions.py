ca6b5c0decfb72f82a998e8f3af7555c39f01116
Chittu13 <shanmukha.s.royal@gmail.com>
2022-10-23 08:44:16 +0530

    Modified code :)

--- Problems/questions.py
+++ Problems/questions.py
@@ -1,18 +1,12 @@
-1. write a program to find most repected characters in a string   
+# 1. write a program to find most repected characters in a string
 
-st=input("Enter the string: ")  #Input form the user
-x={}  #First we need to take empty dictionary
-for i in st:
-    if i in x:
-        x[i]+=1
+string = input("Enter the string: ")  # Input form the user
+Empty = {}  # First we need to take empty dictionary
+for i in string:
+    if i in Empty:
+        Empty[i] += 1
     else:
-        x[i]=1
+        Empty[i] = 1
 
-y=sorted(x.items(),key=lambda x:x[1],reverse=True)
-print(y)
-
-try:
-  x=int(input("Enter the age: "))
-  print(x)
-except ValueError:
-    print("Enter the correct age")
+orginal = sorted(Empty.items(), key=lambda Empty: Empty[1], reverse=True)
+print(f' Most Repected Character: {orginal[0][0]} ---> {orginal[0][1]}')
