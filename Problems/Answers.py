f3006e983625da28fc375f899a82ab606ef090b4
Chittu13 <shanmukha.s.royal@gmail.com>
2022-10-23 17:58:12 +0530

    Updated :)

--- Problems/Answers.py
+++ Problems/Answers.py
@@ -1,9 +1,9 @@
 # 1. write a program to find most repected characters in a string
 
 string = input("Enter the string: ")  # Input form the user
-Empty = {}  # First we need to take empty dictionary
+Empty = {}                            # First we need to take empty dictionary
 for i in string:
-    if i in Empty:
+    if i in Empty:                    # Checking the character in the Empty set is there are not
         Empty[i] += 1
     else:
         Empty[i] = 1
