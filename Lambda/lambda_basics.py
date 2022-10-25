a0c64be37aab6611dfb4767a36ee51a76c220f64
Chittu13 <shanmukha.s.royal@gmail.com>
2022-10-25 10:32:24 +0530

    lambda functions

--- Lambda/lambda_basics.py
+++ Lambda/lambda_basics.py
@@ -1,22 +1,21 @@
-# Basic of lambda function
 
-add=lambda a,b:a+b
-sub=lambda a,b:a-b
-mul=lambda a,b:a*b
-div=lambda a,b:a/b
 
-first=int(input("Enter the first number: "))
-second=int(input("Enter the second number: "))
-per=input('Enter the operation you want to perform like: "+  -  *  /": ')
-if per=="+":
-    print(add(first,second))
-elif per=="-":
-    print(sub(first,second))
-elif per=="*":
-    print(mul(first,second))
-elif per=="/":
-    print(div(first,second))
+def add(a, b): return a+b
+def sub(a, b): return a-b
+def mul(a, b): return a*b
+def div(a, b): return a/b
+
+
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+per = input('Enter the operation you want to perform like: "+  -  *  /": ')
+if per == "+":
+    print(add(first, second))
+elif per == "-":
+    print(sub(first, second))
+elif per == "*":
+    print(mul(first, second))
+elif per == "/":
+    print(div(first, second))
 else:
     print("Please enter the right operation :( ")
-
-
