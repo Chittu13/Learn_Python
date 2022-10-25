5c5728df78c6e4ebc213229f5653745e73428fdd
Chittu13 <shanmukha.s.royal@gmail.com>
2022-10-25 11:07:05 +0530

    lambda function with filter

--- Lambda/lambda_with_filter.py
+++ Lambda/lambda_with_filter.py
@@ -4,7 +4,7 @@ x=[2,3,42,1,1,0,4]
 y=filter(lambda n:n%2==0, x)
 print(list(y))
 
-# program for checking even numbers greater than 2  using lambda filter function
+# program for checking even numbers greater than '2'  using lambda filter function
 
 x=[1,2,3,4,5,6,7,8,9]
 y=filter(lambda a: (a%2==0)and a>2  ,x )
